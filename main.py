1de09b3cee3d7e1748a13d32e6d796d72deb16a3
CourtneyV2025 <courtney.valis2025@gbwl.org>
2024-09-11 14:39:05 +0000

    edit main.py

--- main.py
+++ main.py
@@ -7,6 +7,40 @@ def main():
     is_fave_class = True
     my_grade = 99.99
 
+    # Check the type of a variable
+    type_of_age = type(age)
+    print(type_of_age)
 
+    # String formatting with f-strings
+    #f"String literal {variable}""
+    print(f"the type of the variable age is: {type_of_age}")
+    print(f"My age is 10 years: {age + 10}") 
+
+
+
+def bake_cookie(ingredients, instructions, temperature):
+    # Print the list of ingredients 
+
+    # Print the oven temperature setting
+
+    # Print the instructions 
+    print(instructions) 
+
+    print("String literal",teacher)
 if __name__ == "__main__":
     main()
+
+    # Declare Variables
+    x = 42
+    y = 3 /4 
+    z = int('7')
+    a = float(5)
+    name = "Nina" 
+    
+    # Check the type of variable
+    print(type(x))
+    print(type(y))
+    print(type(z))
+    print(type(a))
+    print(type(name))
+    
