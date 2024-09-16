4e4c885076af412d4d247eb1de00b27149bb74a8
CourtneyV2025 <courtney.valis2025@gbwl.org>
2024-09-16 12:52:58 +0000

    edit main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def main():
    
 #You can also use the argument's keyword. This helps with readability
 print(calculate_numbers(2, 3)) #goes with default, "add"
-print(calculate_numbers(2, 3, "subtract"))
+print(calculate_numbers(2, 3, "subtract")) 
 print(calculate_numbers(2,3,operation="subtract")) # specify keyword
 
 # Different ways to modify values while iterating 
@@ -38,6 +38,48 @@ print(calculate_numbers(2,3,operation="subtract")) # specify keyword
 numbers = [5, 5, 6, 5.5, 7, 42, 70] 
 list_interation(numbers)
 
+    #Test differet container types
+list_demo()
+tuple_demo()
+set_demo()
+dict_demo()
+
+def list_demo():
+    print("***LIST DEMO***")
+    my_list = ["h", "e", "l", "l", "o"]
+
+    # Add an item to a list 
+    my_list.append("!") 
+    print(my_list)
+
+    # Get the number of items
+    print(len(my_list))
+
+    # Use inde to access specific items
+    print(my_list[0])
+    print(my_list[4:6]) # item at index 4 (inclusive) until 6 (excluded)
+    print(my_list[4:1]) # item at 4 until the end
+    # NEGATIVE INDEX is located from the end, back 2 spots
+    print(my_list[-2:1]) # item at 4 until the end 
+
+    # Remove the first 'l'
+    my_list.remove("l")
+    #Insert an item at a inde
+    my_list.insert(1,"l") 
+    print(my_list)
+
+
+
+
+
+def tuple_demo():
+    print("***TUPLE DEMO***")
+
+def set_demo():
+    print("***SET DEMO***")
+
+def dict_demo():
+    print("***DICT DEMO***")
 
 def bake_cookie(ingredients, instructions, temperature, cutter="circle"):
 
@@ -55,14 +97,16 @@ def bake_cookie(ingredients, instructions, temperature, cutter="circle"):
 
 def calculate_numbers(x, y, operation="add"):
     if operation == "add":
-        return x + y
+         return x + y
     elif operation == "subtract":
-        return x - y
+         return x - y
+
 
  # Let's try our new function. Remember, if we don't pass the operation keyword argument, the 
     calculate_numbers(2,3)
 # You can pass a keyword argument as a normal positional argument 
     calculate_numbers(2, 3, "subtract")
+
 
 
 def list_interation(list): 
