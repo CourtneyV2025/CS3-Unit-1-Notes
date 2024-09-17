13e89b48719fe9fa0e4c660650ed4caaf7eed4d9
CourtneyV2025 <courtney.valis2025@gbwl.org>
2024-09-17 19:11:06 +0000

    edited main.py

--- main.py
+++ main.py
@@ -21,9 +21,10 @@ def main():
     ingredients_list = ["chocolate chips", "cinnamon", "flour", "sugar", "butter", "eggs"]
     snickerdoodle = "mix everything and put in oven. there ya go!"
     temperature = 300
+  
 
     # Call a function
-    bake_cookie(ingredients_list, snickerdoodle, temperature, name=recipe_name) 
+    bake_cookie(ingredients_list, snickerdoodle, temperature) 
 
     #Call a function with an optional argument 
     bake_cookie(ingredients_list, snickerdoodle, temperature, "star")
@@ -35,16 +36,17 @@ def main():
 
 # Different ways to modify values while iterating 
 # Note that you can mix data types in the same list!!!!!!
-numbers = [5, 5, 6, 5.5, 7, 42, 70] 
-list_interation(numbers)
+    numbers = [5, 5, 6, 5.5, 7, 42, 70] 
+    list_iteration(numbers)
 
     #Test differet container types
-list_demo()
-tuple_demo()
-set_demo()
-dict_demo()
+    list_demo()
+    tuple_demo()
+    set_demo()
+    dict_demo()
 
 def list_demo():
+    print()
     print("***LIST DEMO***")
     my_list = ["h", "e", "l", "l", "o"]
 
@@ -68,20 +70,57 @@ def list_demo():
     my_list.insert(1,"l") 
     print(my_list)
 
+    # Insert an item at index
+    my_list.insert(1,"1")
+    print(my_list)
+    
+    # Check if a certain value exissts in a list 
+    # x in sequence - boolean expression
+    print("!" in my_list)
+
+    # Sort our list in reverse order 
+    # order meaning "alphabetical"
+    my_list.sort(reverse=True)
+    print(my_list)
+
+def tuple_demo():
+    print()
+    print("***TUPLE DEMO***")
+    # tuples are IMMUTABLE
+    person = ('Courtney', 17, 'Brooklyn, NY')
+    name, age, hometown = person
+    # created multiple variables in one line 
+    print(name)
+    print(age)
+    print(hometown)
+
+
+def set_demo():
+    print()
+    print("***SET DEMO***")
+    my_set = set()
+    my_set = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+    
+    my_set.add(0) # will inset in ORDER
+    my_set.remove(4)
+    print(my_set)
+
+    # Sets can only contain UNIQUE val
+    my_set.add(8)
+    print(my_set)
+    #Useful math operations between two sets 
+    other_set = {2, 4, 6, 8, 10}
+    print(my_set.union(other_set))
+    print(my_set.intersection(other_set))
+    print(my_set.difference(other_set))
 
 
 
+def dict_demo():
+    print()
+    print("***DICT DEMO***")
 
-    def tuple_demo():
-        print("***TUPLE DEMO***")
-
-    def set_demo():
-        print("***SET DEMO***")
-
-    def dict_demo():
-        print("***DICT DEMO***")
-
-    def bake_cookie(ingredients, instructions, temperature, cutter="circle"):
+def bake_cookie(ingredients, instructions, temperature, cutter="circle"):
 
     # Print the list of ingredients 
     for item in ingredients:
@@ -95,10 +134,10 @@ def list_demo():
     print(f"Now cut your cookies with a {cutter} cookie cutter")
     print()
 
-    def calculate_numbers(x, y, operation="add"):
-        if operation == "add":
+def calculate_numbers(x, y, operation="add"):
+    if operation == "add":
          return x + y
-        elif operation == "subtract":
+    elif operation == "subtract":
             return x - y
 
 
@@ -107,19 +146,16 @@ def list_demo():
 # You can pass a keyword argument as a normal positional argument 
     calculate_numbers(2, 3, "subtract")
 
-
-
-    def list_interation(list): 
+def list_iteration(list): 
     # 1. Create a new list as you loop
         new_list = []    
         for item in list:
-        new_list.append(item * 2)
+            new_list.append(item * 2)
         print(new_list)
 
     #2. LIST COMPREHENSION
         input_list = [item * 2 for item in list]
         print(input_list)
-
 
 if __name__ == "__main__":
     main()
@@ -131,9 +167,9 @@ if __name__ == "__main__":
     name = "Nina" 
     
     # Check the type of variable
-    print(type(x))
-    print(type(y))
-    print(type(z))
-    print(type(a))
-    print(type(name))
+    # print(type(x))
+    # print(type(y))
+    # print(type(z))
+    # print(type(a))
+    # print(type(name))
     
